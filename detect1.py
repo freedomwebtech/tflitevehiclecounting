2a8e54727fb6e105211bd853a3d2073bc550f480
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2023-03-13 13:57:45 +0530

    Update detect1.py

--- detect1.py
+++ detect1.py
@@ -20,7 +20,7 @@ import cv2
 from tflite_support.task import core
 from tflite_support.task import processor
 from tflite_support.task import vision
-import utils1
+import utils
 
 
 def run(model: str, camera_id: int, width: int, height: int, num_threads: int,
